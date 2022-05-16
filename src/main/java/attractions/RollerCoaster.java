15f5fb64870be78ab02948568a0a520aaf15e420
thumminbird14 <tanyapurcell@hotmail.co.uk>
2022-05-16 21:12:22 +0100

    commit Rollercoaster changes

--- src/main/java/attractions/RollerCoaster.java
+++ src/main/java/attractions/RollerCoaster.java
@@ -1,8 +1,19 @@
 package attractions;
 
-public class RollerCoaster  extends Attraction {
+import behaviours.ISecurity;
+import people.Visitor;
+
+public class RollerCoaster  extends Attraction implements ISecurity {
 
     public RollerCoaster(String name, int rating) {
         super(name, rating);
+    }
+
+    public boolean isAllowedTo(Visitor visitor){
+        if (visitor.getAge() > 12 && visitor.getHeight() > 145){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,16 +2,26 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor tenYearOld;
+    Visitor tallTenYearOld;
+    Visitor shortThirteenYearOld;
+    Visitor tallThirteenYearOld;
 
     @Before
     public void setUp() {
+
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        tenYearOld = new Visitor(10, 140, 3.85);
+        tallTenYearOld = new Visitor(10, 150, 10.60);
+        shortThirteenYearOld = new Visitor(13, 130, 15.00);
+        tallThirteenYearOld = new Visitor(13, 155, 20.00);
     }
 
     @Test
@@ -27,5 +37,25 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void rejects10YearOld() {
+        assertEquals(false, rollerCoaster.isAllowedTo(tenYearOld));
+    }
+
+    @Test
+    public void rejectsTall10YearOld() {
+        assertEquals(false, rollerCoaster.isAllowedTo(tallTenYearOld));
+    }
+
+    @Test
+    public void rejectsShort13YearOld() {
+        assertEquals(false, rollerCoaster.isAllowedTo(shortThirteenYearOld));
+    }
+
+    @Test
+    public void allowsTall13YearOld() {
+        assertEquals(true, rollerCoaster.isAllowedTo(tallThirteenYearOld));
     }
 }
